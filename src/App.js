50339f2170e8e6effe74bc02ecad87177bd661ba
Prateek Jain <jain.prateek904@gmail.com>
2020-04-19 21:09:49 +0200

    added functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>This is third attempt</h1>
+        <h1>Hi! This is React App</h1>
+        <p>This is actually working!!!</p>
+        <Person />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p>This is person.</p>
+}
+
+export default person;
