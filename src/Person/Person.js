cae2854cc733dbb94cf329c18885e6f9025c43b9
Prateek Jain <jain.prateek904@gmail.com>
2020-04-29 16:23:48 +0200

    css modules for Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,41 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-//import './Person.css';
-
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px
-    }
-`;
+import classes from './Person.css';
 
 const person = (props) => {
-    // const style = {
-    //     '@media (min-width: 500px)' : {
-    //         width: '450px'
-    //     }
-    // }
     return (
-        <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name}. My age is {props.age}.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
+        </div>
     )
 };
 
-
 export default person;
-
-// export const person = () => {
-//     return <p>I'm a person.</p>
-// };
-// 
-// Below will be the import in App.js file
-// import {person as Person} from "./Person/Person";
